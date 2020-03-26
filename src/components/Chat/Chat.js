fd289435617ce163f8aad9a8304cb9e52f10a7d4
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2020-03-26 23:38:00 +0530

    first commit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -14,7 +14,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://chat-app-kaustav.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
